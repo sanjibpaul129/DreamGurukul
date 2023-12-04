0ba5cbde96551ace354fa4f8aa3423957d163875
saikat112 <saikatmanna112@gmail.com>
2023-12-04 15:21:26 +0530

    dubg

---
+++ test_guru_form.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "hello"
+//  $name = $_POST["name"];
+//  $email = $_POST["email"];
+//  $mobile = $_POST["mobile"];
+//  $comments = $_POST["comments"];
+//  $utm_source = $_POST["utm_source"];
+//  $utm_medium = $_POST["utm_medium"];
+//  echo $name;
+//  echo $email;
+//  echo $mobile;
+//  echo $comments;
+//  echo $utm_source;
+//  echo $utm_medium;
+//  header('Location: thankyou.php?email=' . $email . "&contact=" . $contact);
+?>
