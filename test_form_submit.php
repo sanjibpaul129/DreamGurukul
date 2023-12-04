4387580fa22fa7a6e85a72b95993c42529ac08c0
saikat112 <saikatmanna112@gmail.com>
2023-12-04 17:53:54 +0530

    dubg

--- test_form_submit.php
+++ test_form_submit.php
@@ -1,29 +1,22 @@
 <?php
     $name = $_POST["fullname"];
     $email = $_POST["email"];
-    $contact = $_POST["phone"];
+    $mobile = $_POST["phone"];
     $comments = $_POST["comments"];
     $project = "Dream Gurukul";
     $utm_form_name = "Enquiry Form";
     $utm_source = $_POST["utm_source"];
     $utm_medium = $_POST["utm_medium"];
     $apikey = "kG6vgYgUqEmnHUtHX15pNQ";
-    echo $name;
-    echo "=====================";
-    echo $email;
-    echo "=====================";
-    echo $contact;
-    echo "=====================";
-    echo $comments;
-    echo "=====================";
-    echo $project;
-    echo "=====================";
-    echo $utm_form_name;
-    echo "=====================";
-    echo $utm_source;
-    echo "=====================";
-    echo $utm_medium;
-    echo "=====================";
-    echo $apikey;
-    echo "=====================";
+    $url = 'https://www.realtybucket.com/webhook/website_form_data';
+    $data = array('apikey' => $apikey, 'name' => $name, 'email' => $email,'mobile' => $phone, 'comments' => $comments, 'project' => $project, 'utm_form_name' => $utm_form_name, 'utm_source' => $utm_source,'utm_medium' => $utm_medium);
+    $options = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data)
+        )
+    );
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
 ?>
