5a38d5a66aa0410b24f6177a01dfdc395438941f
saikat112 <saikatmanna112@gmail.com>
2023-12-05 14:39:01 +0530

    dubg

--- testimonials_enquiry_form_submit.php
+++ testimonials_enquiry_form_submit.php
@@ -8,17 +8,36 @@
     $utm_source = $_POST["utm_source"];
     $utm_medium = $_POST["utm_medium"];
     $apikey = "kG6vgYgUqEmnHUtHX15pNQ";
+echo $name ;
+echo "==================";
+echo $email ;
+echo "==================";
+echo $mobile ;
+echo "==================";
+echo $comments ;
+echo "==================";
+echo $project ;
+echo "==================";
+echo $utm_form_name ;
+echo "==================";
+echo $utm_source ;
+echo "==================";
+echo $utm_medium ;
+echo "==================";
+echo $apikey ;
+echo "==================";
 
-    $url = 'https://www.realtybucket.com/webhook/website_form_data';
-    $data = array('apikey' => $apikey, 'name' => $name, 'email' => $email,'mobile' => $mobile,'comments' => $comments, 'project' => $project, 'utm_form_name' => $utm_form_name, 'utm_source' => $utm_source,'utm_medium' => $utm_medium);
-    $options = array(
-        'http' => array(
-            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-            'method'  => 'POST',
-            'content' => http_build_query($data)
-        )
-    );
-    $context  = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    header('Location: testimonial_thankyou.php');
+
+    // $url = 'https://www.realtybucket.com/webhook/website_form_data';
+    // $data = array('apikey' => $apikey, 'name' => $name, 'email' => $email,'mobile' => $mobile,'comments' => $comments, 'project' => $project, 'utm_form_name' => $utm_form_name, 'utm_source' => $utm_source,'utm_medium' => $utm_medium);
+    // $options = array(
+    //     'http' => array(
+    //         'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+    //         'method'  => 'POST',
+    //         'content' => http_build_query($data)
+    //     )
+    // );
+    // $context  = stream_context_create($options);
+    // $result = file_get_contents($url, false, $context);
+    // header('Location: testimonial_thankyou.php');
 ?>
