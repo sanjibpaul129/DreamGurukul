7e2692ec5b1ae701a76f23950528040823bb7739
saikat112 <saikatmanna112@gmail.com>
2023-12-06 11:29:24 +0530

    dubg

--- testimonials_enquiry_form_submit.php
+++ testimonials_enquiry_form_submit.php
@@ -1,7 +1,7 @@
 <?php
     $name = $_POST["name"];
     $email = $_POST["email"];
-    $mobile = $_POST["mobile"];
+    $contact = $_POST["mobile"];
     $comments = $_POST["comments"];
     $project = "Dream Gurukul";
     $utm_form_name = "Enquiry Form";
@@ -10,7 +10,7 @@
     $apikey = "kG6vgYgUqEmnHUtHX15pNQ";
 
     $url = 'https://www.realtybucket.com/webhook/website_form_data';
-    $data = array('apikey' => $apikey, 'name' => $name, 'email' => $email,'mobile' => $mobile,'comments' => $comments, 'project' => $project, 'utm_form_name' => $utm_form_name, 'utm_source' => $utm_source,'utm_medium' => $utm_medium);
+    $data = array('apikey' => $apikey, 'name' => $name, 'email' => $email,'mobile' => $contact,'comments' => $comments, 'project' => $project, 'utm_form_name' => $utm_form_name, 'utm_source' => $utm_source,'utm_medium' => $utm_medium);
     $options = array(
         'http' => array(
             'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
