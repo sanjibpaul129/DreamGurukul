ff19f9002a925e978d080946a828358c766e55c9
sanjib <sanjibpaul12012000@gmail.com>
2023-12-29 14:56:29 +0530

    utm_form_name_change

--- formsubmit.php
+++ formsubmit.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $curl = curl_init();
 
   curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://www.realtybucket.com/webhook/website_form_data?apikey=kG6vgYgUqEmnHUtHX15pNQ&name=' . $_POST["fullname"] . '&email=' . $_POST["email"] . '&contact=' . $_POST["phone"] . '&project=Dream%20Gurukul&utm_form_name=Enquiry%20Form&utm_source=&network=test_data&campaign_id=test_data&adgroup_id=&gclid=&device=&creative=&placement=&target_id=&loc_interest_ms=&loc_physical_ms=&device_model=&keyword=&match_type=&mobile=',
+    CURLOPT_URL => 'https://www.realtybucket.com/webhook/website_form_data?apikey=kG6vgYgUqEmnHUtHX15pNQ&name=' . $_POST["fullname"] . '&email=' . $_POST["email"] . '&contact=' . $_POST["phone"] . '&project=Dream%20Gurukul&utm_form_name=In_Enquiry%20Form&utm_source=&network=test_data&campaign_id=test_data&adgroup_id=&gclid=&device=&creative=&placement=&target_id=&loc_interest_ms=&loc_physical_ms=&device_model=&keyword=&match_type=&mobile=',
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => '',
     CURLOPT_MAXREDIRS => 10,
